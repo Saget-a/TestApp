7215ea1c6bf940c45432524846db07dafc8fef0f
Saget <132357928+Saget-a@users.noreply.github.com>
2025-04-09 15:46:40 +0300

    change

--- src/main/java/test/textboxes/Application.java
+++ src/main/java/test/textboxes/Application.java
@@ -9,20 +9,23 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-    //подключение
+
+        //подключение файла разметки
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-
+        //ограничение сцены
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-    //подключение css к основе
+        //подключение css
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-
+        //назввание окна
         stage.setTitle("TEST APP");
+        //применение настроек сцены
         stage.setScene(scene);
+        //отключение изменения розмера окна
         stage.setResizable(false);
+        //запуск сцены
         stage.show();
     }
     public static void main(String[] args) {
         launch();
     }
 }
-// eifojwefjwefiowje
