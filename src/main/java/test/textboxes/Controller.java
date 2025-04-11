0b50357722ff627d2a272a336502a357992082b1
Saget <132357928+Saget-a@users.noreply.github.com>
2025-04-11 12:22:12 +0300

    Передалка логина внутри приложения

--- src/main/java/test/textboxes/Controller.java
+++ src/main/java/test/textboxes/Controller.java
@@ -2,7 +2,10 @@ package test.textboxes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
@@ -12,13 +15,42 @@ public class Controller {
     private TextField inputField;
 
     @FXML
+    private VBox loginPane;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label loginError;
+
+    @FXML
     protected void onHelloButtonClick() {
         try {
             int number = Integer.parseInt(inputField.getText());
             int result = integer.square(number);
-            welcomeText.setText("number; " + result);
+            welcomeText.setText("number: " + result);
         } catch (NumberFormatException e) {
             welcomeText.setText("Пожалуйста, введите корректное число");
+        }
+    }
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if ("admin".equals(username) && "1234".equals(password)) {
+            loginPane.setVisible(false);
+            mainPane.setVisible(true);
+            welcomeText.setText("Добро пожаловать, " + username + "!");
+        } else {
+            loginError.setText("Неверный логин или пароль");
         }
     }
 }
